359d5080ad02f84e05c842923bb38446c6ce91d7
Ki.BongJo <Kibongjo1017@gmail.com>
2020-09-25 10:24:26 +0900

    Updates

---
+++ precache-manifest.2200218065147e5fecd821184907238d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882334b5bd1c0c52c1619e374bbb58c9",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4a474b10bc559f07352d",
+    "url": "/movie_app/static/css/main.0aab021c.chunk.css"
+  },
+  {
+    "revision": "990e9222a6eb1a0d1c2b",
+    "url": "/movie_app/static/js/2.02cb4e9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.02cb4e9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a474b10bc559f07352d",
+    "url": "/movie_app/static/js/main.5d3e8cc8.chunk.js"
+  },
+  {
+    "revision": "01c2ff2572678c311c7b",
+    "url": "/movie_app/static/js/runtime-main.b3d29ec4.js"
+  }
+]);
